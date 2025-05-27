fdd11471d14974815fe29976c9e609d414d8663a
Yannick Koller <47781849+PossessedC0bra@users.noreply.github.com>
2025-05-27 23:18:27 +0200

    feat: improve placeholder detection
    - make placeholder detection stricter (allow only alphanumeric characters and '-' and '_' in placeholder names)

--- src/lib/placeholder/placeholderUtil.ts
+++ src/lib/placeholder/placeholderUtil.ts
@@ -1,6 +1,6 @@
 const PLACEHOLDER_PREFIX = "{{";
 const PLACEHOLDER_SUFFIX = "}}";
-const GLOBAL_PLACEHOLDER_REGEX = new RegExp(`(${PLACEHOLDER_PREFIX}.*?${PLACEHOLDER_SUFFIX})`, 'g');
+const GLOBAL_PLACEHOLDER_REGEX = new RegExp(`(${PLACEHOLDER_PREFIX}[a-zA-Z0-9-_]+${PLACEHOLDER_SUFFIX})`, 'g');
 
 const toPlaceholderName = (placeholder: string): string => placeholder.replace(PLACEHOLDER_PREFIX, "").replace(PLACEHOLDER_SUFFIX, "");
 
