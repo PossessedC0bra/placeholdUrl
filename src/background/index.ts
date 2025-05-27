299c63312c31fc4869eb6f2f47bcc8a2c93d292d
Yannick Koller <47781849+PossessedC0bra@users.noreply.github.com>
2025-05-27 23:15:38 +0200

    feat: improve automatic popup
    - open when activating or updating tab with unresolved placeholders

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,11 +1,26 @@
 import {GLOBAL_PLACEHOLDER_REGEX} from "@/lib/placeholder/placeholderUtil.ts";
 
-chrome.tabs.onUpdated.addListener(async (_tabId, changeInfo, tab) => {
-    if (changeInfo.status !== "loading" || !tab?.url) return;
+function openPopupOnPlaceholderDetection(tab: chrome.tabs.Tab) {
+    if (!tab.url) {
+        return;
+    }
 
     const plainUrl = decodeURIComponent(tab.url);
     const matches = plainUrl.match(GLOBAL_PLACEHOLDER_REGEX);
     if (matches && matches.length > 0) {
-        await chrome.action.openPopup();
+        chrome.action.openPopup().catch(console.log);
     }
+}
+
+chrome.tabs.onActivated.addListener(async ({tabId}) => {
+    const tab = await chrome.tabs.get(tabId);
+    openPopupOnPlaceholderDetection(tab);
+});
+
+chrome.tabs.onUpdated.addListener(async (_tabId, _changeInfo, tab) => {
+    if (!tab.active) {
+        return;
+    }
+
+    openPopupOnPlaceholderDetection(tab);
 });
