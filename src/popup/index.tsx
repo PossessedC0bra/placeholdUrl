fcb20184ebb319a84fd3b45354c8f0129ddf5bd1
Yannick Koller <47781849+PossessedC0bra@users.noreply.github.com>
2025-05-27 23:19:00 +0200

    feat: add automatic dark / light mode

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,9 +2,12 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import Popup from './Popup.tsx'
+import {ThemeProvider} from "@/components/ThemeProvider.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Popup/>
+        <ThemeProvider>
+            <Popup/>
+        </ThemeProvider>
     </StrictMode>,
 )
