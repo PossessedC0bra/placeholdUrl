ce3d49db2e3053e3b0bab0a7ea91fe78a08778b4
Yannick Koller <47781849+PossessedC0bra@users.noreply.github.com>
2025-06-20 19:25:01 +0200

    fix(popup): fix input border clipping and improve layout

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,5 +1,4 @@
 import {useEffect, useState} from "react";
-import {ScrollArea} from "@/components/ui/scroll-area";
 import {Badge} from "@/components/ui/badge";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
@@ -22,33 +21,26 @@ function extractPlaceholders(url: string): Record<string, string> {
 
 function Popup() {
     const [tab, setTab] = useState<chrome.tabs.Tab | null>(null);
-    useEffect(() => {
-        (async () => {
-            const [activeTab] = await chrome.tabs.query({active: true, currentWindow: true});
-            setTab(activeTab);
-        })();
-    }, []);
-
     const [plainUrl, setPlainUrl] = useState("");
     const [urlParts, setUrlParts] = useState<string[]>([]);
     const [placeholderValueMap, setPlaceholderValueMap] = useState({} as Record<string, string>);
+
     useEffect(() => {
-        if (!tab) {
-            return;
-        }
+        (async () => {
+            const [activeTab] = await chrome.tabs.query({active: true, currentWindow: true});
+            if (!activeTab || !activeTab.url) {
+                console.error("No active tab with a valid URL found.");
+                return;
+            }
 
-        const plainUrl = decodeURIComponent(tab.url!);
-        setPlainUrl(plainUrl);
-        console.log(`Plain URL: ${plainUrl}`);
+            setTab(activeTab);
 
-        const urlParts = plainUrl.split(GLOBAL_PLACEHOLDER_REGEX);
-        setUrlParts(urlParts);
-        console.log(`URL Parts: `, urlParts);
-
-        const placeholderValueMap = extractPlaceholders(plainUrl);
-        setPlaceholderValueMap(extractPlaceholders(plainUrl));
-        console.log(`Placeholder Value Map: `, placeholderValueMap);
-    }, [tab]);
+            const plainUrl = decodeURIComponent(activeTab.url!);
+            setPlainUrl(plainUrl);
+            setUrlParts(plainUrl.split(GLOBAL_PLACEHOLDER_REGEX));
+            setPlaceholderValueMap(extractPlaceholders(plainUrl));
+        })();
+    }, []);
 
     const handlePlaceholderChange = (key: string, value: string) => setPlaceholderValueMap({
         ...placeholderValueMap,
@@ -69,71 +61,65 @@ function Popup() {
     };
 
     return (
-        <form onSubmit={handleReplace} className="m-4 space-y-4">
-            <div className="flex gap-2">
+        <form onSubmit={handleReplace} className="w-[500px] max-h-[600px] py-3 flex flex-col gap-4">
+            {/* Header */}
+            <div className="mx-3 flex items-center gap-2">
                 <Replace/>
                 <span className="text-2xl font-bold">PlaceholdURL</span>
             </div>
 
-            <div>
-                <div className="text-sm font-medium mb-2">URL Preview</div>
+            {/* URL Preview */}
+            <code className="bg-muted mx-3 p-3 overflow-x-auto rounded-md text-xs text-nowrap leading-relaxed">
+                {urlParts
+                    .map((part, index) => {
+                        const match = part.match(GLOBAL_PLACEHOLDER_REGEX);
+                        if (!match) {
+                            return <span key={index}>{part}</span>;
+                        }
 
-                <div className="bg-muted p-3 rounded-md border max-h-20 overflow-x-auto">
-                    <code className="text-xs text-nowrap leading-relaxed">
-                        {urlParts
-                            .map((part, index) => {
-                                const match = part.match(GLOBAL_PLACEHOLDER_REGEX);
-                                if (!match) {
-                                    return <span key={index}>{part}</span>;
-                                }
+                        return <Badge
+                            key={index}
+                            variant={placeholderValueMap[part] ? "lightBlue" : "lightRed"}
+                        >
+                            {placeholderValueMap[part] || part}
+                        </Badge>
 
-                                return <Badge
-                                    key={index}
-                                    variant={placeholderValueMap[part] ? "lightBlue" : "lightRed"}
-                                >
-                                    {placeholderValueMap[part] || part}
-                                </Badge>
-
-                            })}
-                    </code>
-                </div>
-            </div>
+                    })}
+            </code>
 
             {/* Placeholder Fields */}
-            <ScrollArea className="[&>[data-radix-scroll-area-viewport]]:max-h-[225px]">
-                <div className="space-y-2">
-                    {Object.keys(placeholderValueMap).length === 0
-                        ? (
-                            <div className="flex items-center text-sm text-muted-foreground">
-                                No placeholders
-                                found
-                            </div>
-                        )
-                        : (
-                            Object.keys(placeholderValueMap)
-                                .map((placeholder, idx) => (
-                                    <div key={placeholder} className="space-y-1">
-                                        <Label htmlFor={placeholder}>
-                                            {toPlaceholderName(placeholder)}
-                                        </Label>
-                                        <Input
-                                            id={placeholder}
-                                            value={placeholderValueMap[placeholder] || ""}
-                                            onChange={(e) => handlePlaceholderChange(placeholder, e.target.value)}
-                                            autoFocus={idx === 0}
-                                        />
-                                    </div>
-                                ))
-                        )
-                    }
-                </div>
-            </ScrollArea>
+            <div className="px-3 pb-1 overflow-y-auto space-y-2">
+                {Object.keys(placeholderValueMap).length === 0
+                    ? (
+                        <div className="flex justify-center text-sm text-muted-foreground">
+                            No placeholders
+                            found
+                        </div>
+                    )
+                    : (
+                        Object.keys(placeholderValueMap)
+                            .map((placeholder, idx) => (
+                                <div key={placeholder} className="space-y-1">
+                                    <Label htmlFor={placeholder}>
+                                        {toPlaceholderName(placeholder)}
+                                    </Label>
+                                    <Input
+                                        id={placeholder}
+                                        value={placeholderValueMap[placeholder] || ""}
+                                        onChange={(e) => handlePlaceholderChange(placeholder, e.target.value)}
+                                        autoFocus={idx === 0}
+                                    />
+                                </div>
+                            ))
+                    )
+                }
+            </div>
 
             {/* Replace Button */}
             <Button
                 type="submit"
                 disabled={Object.keys(placeholderValueMap).length === 0 || Object.values(placeholderValueMap).some(v => v.trim() === "")}
-                className="w-full"
+                className="mx-3"
             >
                 <ExternalLink/>
                 Open
